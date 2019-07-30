3fced8d73b5ba3a7772c3d04a77c0704b8bcbbf7
miroslaw.gadek <miroslaw.gadek@gmail.com>
2019-07-30 19:58:32 +0200

    Klassen erstellt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace ReadGaebFiles
     {
         static void Main(string[] args)
         {
+            string file = File.ReadAllText(@"");
         }
     }
 }
